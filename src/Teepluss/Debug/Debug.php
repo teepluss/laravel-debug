b321d444f27f818300cc02288fe3d426fd4b1634
Tee <admin@jquerytips.com>
2013-03-27 01:12:10 +0700

    fixed

--- src/Teepluss/Debug/Debug.php
+++ src/Teepluss/Debug/Debug.php
@@ -1,12 +1,58 @@
 <?php namespace Teepluss\Debug;
 
-use dump_r\Core as Dumpper;
+use dump_r\Type, dump_r\Rend;
+use dump_r\Core as BaseDump;
 
-class Debug {
+class Debug extends BaseDump {
 
-	public function dump($raw, $ret = false, $html = true, $depth = 1e3, $expand = 1e3)
+	/**
+	 * Fixed line error.
+	 *
+	 * @param  mixed   $raw
+	 * @param  boolean $ret
+	 * @param  boolean $html
+	 * @param  integer $depth
+	 * @param  integer $expand
+	 * @return string
+	 */
+	public static function dump_r($raw, $ret = false, $html = true, $depth = 1e3, $expand = 1e3)
 	{
-		return Dumpper::dump_r($raw, $ret, $html, $depth, $expand);
+		$root = Type::fact($raw, $depth);
+
+		// get the input arg passed to the function
+		$src = debug_backtrace();
+
+		$idx = ( ! isset($src[0]['file'])) ? 3 : (strpos($src[0]['file'], 'Debug.php') ? 2 : 1);
+
+
+		$src = (object)$src[$idx];
+		$file = file($src->file);
+
+		$line = $file[$src->line - 1];
+
+		preg_match('~(dump|dump_r)\((.+?)(?:,|\)(;|\?>))~', $line, $m);
+
+		$key = $m[2];
+
+		if (PHP_SAPI == 'cli' || !$html)
+			$out = Rend::text0($src->file, $src->line, $key, $root);
+		else
+			$out = Rend::html0($src->file, $src->line, $key, $root, $expand);
+
+		if ($ret)
+			return $out;
+
+		echo $out;
+	}
+
+	/**
+	 * In alias of dump_r
+	 *
+	 * @return dump_r
+	 */
+	public function dump()
+	{
+		return call_user_func_array(array($this, 'static::dump_r'), func_get_args());
 	}
 
 }
